9f5a2f73b776b505222e6560ac949fdb9df6fd80
DiDavilaG <53787782+DiDavilaG@users.noreply.github.com>
2020-04-26 22:57:57 -0500

    Ecuador masp tested with subsets

--- Ecuador_Code.R
+++ Ecuador_Code.R
@@ -1,5 +1,46 @@
 #Using the tutorial to plot Ecuador maps 
+x <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "tmap", "maps","sf", "sp","tidyverse", "ggplot2" )
+#install.packages(x) # warning: uncommenting this may take a number of minutes
+lapply(x, library, character.only = TRUE) # load the required packages
 
-library(rgdal)
-ECpa <- readOGR(dsn = "data/SHP/nxparroquias.shp")
-# lnd <- readOGR(dsn = "data", layer = "london_sport")
+
+
+
+
+dfrecent<- readOGR(dsn="data/ecu_adm_inec_20190724_shp/ecu_admbnda_adm1_inec_20190724.shp")
+
+
+ECpr <- readOGR(dsn = "data/ECU_adm/ECU_adm1.shp")
+ECPRPK<- merge(ECpr, PK_PREF2000, by = "ID_1")
+names(ECPRPK)
+
+#see data 
+
+head(dfrecent)
+
+head(dfrecent@data, n=2)
+
+#check the class of data 
+sapply(dfrecent@data, class)
+#PLOT COUNTRY 
+plot (dfrecent)
+
+#Show a row from the data section 
+ECPRPK@data[ECPRPK$NAME_1 == "Azuay", ]
+# merge files with attributes 
+# import file first csv with same colum name 
+PREFECTOSPKMAP<- merge(dfrecent, PK_PREF2000, by= "ADM1_PCODE")
+
+#subset the spatial section using attributes 
+ECCONT<- subset (PREFECTOSPKMAP, CONTINENT == "yes")
+IND20 <- PREFECTOSPKMAP$IND_PER > 20 & PREFECTOSPK$IND_PER < 100
+EL2000<- ECCONT$PREFECTS_PK2000 == "yes"
+
+plot(PREFECTOSPKMAP[ECCONT,])
+plot(PREFECTOSPKMAP[EL2000,])
+
+
+# subset with context  - takes a long time. 
+plot(ECCONT)
+plot(ECCONT[EL2000,], col = "lightgrey", add = TRUE) # add selected zones to map
+
--- Tutorial_Code.R
+++ Tutorial_Code.R
@@ -1,8 +1,32 @@
 ## Starting the tutorial by loading the packages 
 
 x <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "tmap")
-# install.packages(x) # warning: uncommenting this may take a number of minutes
+install.packages(x) # warning: uncommenting this may take a number of minutes
 lapply(x, library, character.only = TRUE) # load the required packages
 
 library(rgdal)
 lnd <- readOGR(dsn = "data/london_sport.shp")
+#The structure of spatial data in R
+# Spatial objects like the `lnd` object are made up of a number of different *slots*, the key *slots* being `@data` (non geographic *attribute data*) and `@polygons` (or `@lines` for line data). The data *slot* can be thought of as an attribute table and the geometry *slot* is the polygons that make up the physcial boundaries. Specific *slots* are accessed using the `@` symbol. Let's now analyse the sport object with some basic commands:
+head(lnd)
+
+head(lnd@data, n = 2)
+
+# check the classes of all variables 
+
+sapply(lnd@data, class)
+
+# everything is characters, execpt partic_per this needs to be changed 
+
+lnd$Pop_2001 <- as.numeric(as.character(lnd$Pop_2001))
+
+plot(lnd) 
+#subset a section and plot 
+sel <- lnd$Partic_Per > 20 & lnd$Partic_Per < 25
+plot(lnd[sel, ]) # output not shown here
+#explore how it looks 
+head(sel)
+## add context
+plot(lnd, col = "lightgrey") # plot the london_sport object
+sel <- lnd$Partic_Per > 25
+plot(lnd[ sel, ], col = "blue", add = TRUE) # add selected zones to map
